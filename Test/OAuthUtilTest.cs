8abb50247ff68d75e5a046e92ea1992790deed62
Eamon Doyle <eamon.doyle@mastercard.com>
2016-06-18 18:35:23 +0100

    switch test key to key created under APISupport@mastercard.com

--- Test/OAuthUtilTest.cs
+++ Test/OAuthUtilTest.cs
@@ -21,7 +21,7 @@ namespace TestMasterCard
 		{
 
             var currentPath = MasterCard.Core.Util.GetCurrenyAssemblyPath();
-            var authentication = new OAuthAuthentication("gVaoFbo86jmTfOB4NUyGKaAchVEU8ZVPalHQRLTxeaf750b6!414b543630362f426b4f6636415a5973656c33735661383d", currentPath + "\\Test\\prod_key.p12", "alias", "password");
+            var authentication = new OAuthAuthentication("L5BsiPgaF-O3qA36znUATgQXwJB6MRoMSdhjd7wt50c97279!50596e52466e3966546d434b7354584c4975693238513d3d", currentPath + "\\Test\\mcapi_sandbox_key.p12", "alias", "password");
 			ApiConfig.setAuthentication (authentication);
 		}
 
