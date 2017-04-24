20ede841208948219b143d429ab567d85124ecfb
Anthony Rainer <0@0ze.ro>
2017-04-24 09:13:47 -0500

    Added constructors to security classes to allow passing in certificate/key data via a byte array and passing in X509Certificate2 flags.

--- Sources/MasterCard/Core/Security/OAuth/OAuthAuthentication.cs
+++ Sources/MasterCard/Core/Security/OAuth/OAuthAuthentication.cs
@@ -54,16 +54,40 @@ namespace MasterCard.Core.Security.OAuth
 			}
 		}
 
-		public OAuthAuthentication(String clientId, String filePath, String alias, String password)
+    /// <summary>
+    /// Create a new instance of OAuthAuthentication using a file path for certificate data.
+    /// </summary>
+    /// <param name="clientId">The ClientId (Consumer Key) for MasterCard API.</param>
+    /// <param name="filePath">The file path to the certificate data file (*.p12) for authenticating to MasterCard API via OAuth.</param>
+    /// <param name="alias">The key alias. This is not used internally so passing in null or an empty string is ok. NOTE: This may be required in the future.</param>
+    /// <param name="password">The private key password.</param>
+    /// <param name="keyStoreFlags">Any additional flags that need to be passed to <see cref="X509Certificate2"/>.  Azure (App Service, Cloud Service, VM, etc.) requires that <see cref="X509KeyStorageFlags.MachineKeySet"/> is used.</param>
+		public OAuthAuthentication(String clientId, String filePath, String alias, String password, X509KeyStorageFlags keyStoreFlags = X509KeyStorageFlags.DefaultKeySet)
 		{
-			cert = new X509Certificate2(filePath, password, X509KeyStorageFlags.Exportable);
+			cert = new X509Certificate2(filePath, password, keyStoreFlags | X509KeyStorageFlags.Exportable);
 
 			privateKey = cert.PrivateKey;
 			this.clientId = clientId;
 			encoder =  new UTF8Encoding();
 		}
 
-		public void SignRequest(Uri uri, IRestRequest request) {
+    /// <summary>
+    /// Create a new instance of OAuthAuthentication using a byte array for certificate data.
+    /// </summary>
+    /// <param name="clientId">The ClientId (Consumer Key) for MasterCard API.</param>
+    /// <param name="rawCertificateData">The raw certificate data for authenticating to MasterCard API via OAuth.</param>
+    /// <param name="alias">The key alias. This is not used internally so passing in null or an empty string is ok. NOTE: This may be required in the future.</param>
+    /// <param name="password">The private key password.</param>
+    /// <param name="keyStoreFlags">Any additional flags that need to be passed to <see cref="X509Certificate2"/>.  Azure (App Service, Cloud Service, VM, etc.) requires that <see cref="X509KeyStorageFlags.MachineKeySet"/> is used.</param>
+    public OAuthAuthentication(String clientId, byte[] rawCertificateData, String alias, String password, X509KeyStorageFlags keyStoreFlags = X509KeyStorageFlags.DefaultKeySet) {
+      cert = new X509Certificate2(rawCertificateData, password, keyStoreFlags | X509KeyStorageFlags.Exportable);
+
+      privateKey = cert.PrivateKey;
+      this.clientId = clientId;
+      encoder = new UTF8Encoding();
+    }
+
+    public void SignRequest(Uri uri, IRestRequest request) {
 			String uriString = uri.ToString ();
 			String methodString = request.Method.ToString();
 			//String bodyString = (String) request.Parameters.FirstOrDefault (p => p.Type == ParameterType.RequestBody).Value;
